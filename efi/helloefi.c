cb49cbb38cc368e33912b728ff88f6dfc0a45212
w-mj <wmj@alphamj.cn>
2019-12-26 20:52:39 +0800

    more run

--- efi/helloefi.c
+++ efi/helloefi.c
@@ -1,11 +1,25 @@
 #include <efi.h>
 #include <efilib.h>
  
-EFI_STATUS
-EFIAPI
-efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
-{
-    InitializeLib(ImageHandle, SystemTable);
-    Print(L"Hello, world!\n");
-    return EFI_SUCCESS;
+//EFI_STATUS
+//EFIAPI
+//efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
+//{
+//    InitializeLib(ImageHandle, SystemTable);
+//    Print(L"Hello, world!\n");
+//    return EFI_SUCCESS;
+//}
+//
+//
+EFI_STATUS UefiMain(IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE *SystemTable) {
+	EFI_STATUS Status;
+	UINTN Index;
+	EFI_INPUT_KEY Key;
+	CHAR16 StrBuffer[3] = {0};
+	SystemTable->BootServices->WaitForEvent(1, &SystemTable->ConIn->WaitForKey, &Index);
+	Status = SystemTable->ConIn->ReadKeyStroke(SystemTable->ConIn, &Key);
+	StrBuffer[0] = Key.UnicodeChar;
+	StrBuffer[1] = '\n';
+	SystemTable->ConOut->OutputString(SystemTable->ConOut, StrBuffer);
+	return Status;
 }
--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-qemu-system-x86_64 -cpu qemu64 -bios OVMF.fd -drive file=buildtools/boot.img,if=ide -net none
+make
+make install
+./mkboot.sh
+
+qemu-system-x86_64 -cpu qemu64 -bios OVMF.fd -drive file=boot.img,if=ide -net none
 
