b7707ad3a7105787060800d31e1e0fc751b0f74d
w-mj <wmj@alphamj.cn>
2020-01-07 21:30:57 +0800

    start up

--- boot/README.md
+++ boot/README.md
@@ -1,3 +1,7 @@
 EFI启动模块
 这个模块实际上是一个EDK2的工程
 需要通过EDK2进行编译，生成的boot.efi放在build/boot/中
+
+
+构建编译环境
+https://wiki.osdev.org/GCC_Cross-Compiler
--- boot/boot.c
+++ boot/boot.c
@@ -1,11 +1,15 @@
 #include <Uefi.h>
-//#include <Library/PcdLib.h>
-//#include <Library/UefiLib.h>
-//#include <Library/UefiApplicationEntryPoint.h>
 
 EFI_STATUS
 EFIAPI
-UefiMain (IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE *st) {
-	st->ConOut->OutputString(st->ConOut, L"I am SB\n");
+UefiMain (IN EFI_HANDLE ih, IN EFI_SYSTEM_TABLE *st) {
+	UINTN index;
+	EFI_INPUT_KEY key;
+	CHAR16 strBuffer[3] = {};
+	st->BootServices->WaitForEvent(1, &st->ConIn->WaitForKey, &index);
+	st->ConIn->ReadKeyStroke(st->ConIn, &key);
+	strBuffer[0] = key.UnicodeChar;
+	strBuffer[1] = '\n';
+	st->ConOut->OutputString(st->ConOut, strBuffer);
 	return EFI_SUCCESS;
 }
