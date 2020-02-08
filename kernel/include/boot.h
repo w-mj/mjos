a5a6f0995557a7abb535493f6cc3dd3bad55c60f
w-mj <wmj@alphamj.cn>
2020-02-08 12:08:12 +0800

    reorganized project

--- kernel/include/boot.h
+++ kernel/include/boot.h
@@ -1,11 +1,8 @@
 #pragma once
 #include <types.h>
-#include <memory.h>
-#include <attribute.h>
-
 #include <multiboot.h>
 
-
+#define PAGESIZE 4096
 extern u64 KERNEL_VMA, KERNEL_LMA;
 #define ABSOLUTE(x) (((u64)(x) - ((u64)&KERNEL_VMA)) + ((u64)&KERNEL_LMA))
 #define VIRTUAL(x)  (((u64)(x) + (((u64)&KERNEL_VMA)) - ((u64)&KERNEL_LMA)))
